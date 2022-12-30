6a0ca2f2e6249a41ee269d26261c9bf2163eb9ac
Dries <driesvanhool@gmail.com>
2022-12-30 09:38:08 +0100

    props

--- src/main/java/com/switchfully/eurder/domain/security/KeyCloakConfig.java
+++ src/main/java/com/switchfully/eurder/domain/security/KeyCloakConfig.java
@@ -4,15 +4,20 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
 
 public class KeyCloakConfig {
+
+
     static Keycloak keycloak = null;
     final static String serverUrl = "https://keycloak.switchfully.com/auth/";
     public final static String realm = "java-oct-2022";
     final static String clientId = "eurder-dries";
-    final static String clientSecret = "HhEMkchwPljhhzs6osa4nlb8GWiZiJoE";
+    private static String clientSecret = "HhEMkchwPljhhzs6osa4nlb8GWiZiJoE";
     final static String userName =  "admin";
     final static String password = "pwd";
+
+
 
     public static Keycloak getInstance(){
         if(keycloak == null){
